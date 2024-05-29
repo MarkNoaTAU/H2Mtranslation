f4acbf43c8b8b6d00d50604adb7fa38cc9fd43a2
MarkNoaTAU <noamark@mail.tau.ac.il>
2024-05-29 11:23:51 +0300

    add files

---
+++ data/preprocessors.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_rare_metabolite_statistics(metabolite_features):
+    print(f"Are there nan values: {metabolite_features.isna().any().any()}. Filling any nans with zeros.")
+    plt.rc('xtick', labelsize=6)
+    (((metabolite_features.shape[0] - (metabolite_features.round(decimals=8) == 0).sum(axis=0))
+      / metabolite_features.shape[0]) * 100).round(0).sort_values(
+        ascending=True).plot.bar(figsize=(16, 6), title='% samples non-zero value observed, per metabolite.')
+    plt.show()
+    (metabolite_features.shape[0] - (metabolite_features.round(decimals=8) == 0).sum(axis=0)
+     ).sort_values(ascending=True).plot.bar(
+        figsize=(16, 6), title='#number of samples non-zero value observed, per metabolite.')
+    plt.show()
+
+
+def preprocess_filter_rare_metabolite(metabolite_features: pd.DataFrame, verbose=True, percentage=85) -> pd.DataFrame:
+    """
+    Preprocess the metabolite: remove rare taxa.
+    Metabolites that was found in <percentage of the samples is being removed.
+    If verbose, plot and print statistics.
+
+    :param metabolite_features:         (pd.DataFrame) Observed .
+    :param verbose:                     (bool) Whether to plot and print statistics on the data.
+    :param percentage:                  (int) The minimal percentage of samples to require.
+    :return:                            (pd.DataFrame) Observed, with rare metabolite removed.
+    """
+    min_number_of_samples = int((metabolite_features.shape[0] / 100) * percentage)
+    if verbose:
+        plot_rare_metabolite_statistics(metabolite_features)
+    non_rare_columns = metabolite_features.columns[((metabolite_features.shape[0] - (
+                metabolite_features.round(decimals=8) == 0).sum(axis=0)) >= min_number_of_samples)]
+    if verbose:
+        print(f"There are {len(non_rare_columns)} metabolite with sufficient number of samples "
+              f"(>{percentage}%) out of {metabolite_features.shape[1]} metabolites.")
+    return metabolite_features[non_rare_columns]
+
+
+class MetabolitePreprocessor(object):
+    _HMDB_IDS_TO_NAME_PATH = '/home/noa/lab_code/H2Mtranslation/data/map_hmdb_id_to_name.pkl'
+    _HMDB_NAMES_DESCRIPTION_PATH = '/home/noa/lab_code/H2Mtranslation/data/hmdb_name_and_description.pkl'
+
+    """
+    Metabolite preprocessor.
+    Assuming the data is already processed in some basic way (Not raw metabolite features), including
+    pqn normalization (probabilistic quotient normalization to an internal standard (m/z 278.189; real time, 3.81 min)
+    for subsequent analysis: https://pubmed.ncbi.nlm.nih.gov/16808434/) and alike.
+
+    Preprocesses metabolite data, to enable valid analysis:
+        dropping rare metabolites (metabolites with not enough samples for analysis)
+        preform log transform
+        handle missing values/ zero values (assuming there is a detection threshold, and therefore, there are not
+        really zero values in the samples, but very low values. Usually common setting for nans or zeros in this case
+         to half of the min value per metabolite.)
+         
+         If HMDB, map from HMDB ids to names. 
+    """
+    def __init__(self, verbose=True, percentage=85):
+        self.verbose = verbose
+        self.percentage = percentage
+
+    def preprocess(self, metabolite_features, is_hmdb=True):
+        metabolite_features = preprocess_filter_rare_metabolite(metabolite_features, self.verbose, self.percentage)
+        min_value_per_metabolite = metabolite_features.replace(to_replace=0, value=np.nan).min(axis=0) / 2
+        metabolite_features.replace(to_replace=np.nan, value=0, inplace=True)
+        metabolite_features.replace(to_replace=0, value=min_value_per_metabolite, inplace=True)
+        metabolite_features = metabolite_features.apply(lambda x: np.log(x + 1))
+        if is_hmdb:
+            map_hmdb_id_to_name = pd.read_pickle(MetabolitePreprocessor._HMDB_IDS_TO_NAME_PATH)
+            metabolite_features = metabolite_features.rename(columns=map_hmdb_id_to_name)
+        return metabolite_features
---
+++ data/taxonomy_normalizer.py
@@ -0,0 +1,145 @@
+import pandas as pd
+from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
+
+
+def plot_rare_taxa_statistics(taxa):
+    plt.rc('xtick', labelsize=6)
+    (((taxa.shape[0] - (taxa.round(decimals=8) == 0).sum(axis=0)) / taxa.shape[0]) * 100).round(0).sort_values(
+        ascending=True).plot.bar(figsize=(16, 6), title='% samples non-zero value observed, per taxa.')
+    plt.show()
+    (taxa.shape[0] - (taxa.round(decimals=8) == 0).sum(axis=0)).sort_values(ascending=True).plot.bar(
+        figsize=(16, 6), title='#number of samples non-zero value observed, per taxa.')
+    plt.show()
+    (taxa.shape[0] - (taxa.round(decimals=8) == 0).sum(axis=0)).plot.hist(
+        title='number of non-zero samples per genus')
+    plt.show()
+
+
+def preprocess_filter_rare_taxa(taxa: pd.DataFrame, verbose=True, percentage=10) -> pd.DataFrame:
+    """
+    Preprocess the taxonomy: remove rare taxa.
+    Taxa that was found in <percentage of the samples is being removed.
+    Here, we filter according to non-zero values, on observed abundance, but it's more common to filter to
+    some very small threshold on relative abundance to be less sensitive to sampling noise.
+    If verbose, plot and print statistics.
+
+    :param taxa:        (pd.DataFrame) Observed abundance.
+    :param verbose:     (bool) Whether to plot and print statistics on the data.
+    :param percentage:  (int) The minimal percentage of samples to require.
+    :return:            (pd.DataFrame) Observed abundance, with rare taxa removed.
+    """
+    min_number_of_samples = int((taxa.shape[0] / 100) * percentage)
+    if verbose:
+        plot_rare_taxa_statistics(taxa)
+        print(
+            f"The number of genus/features that have non-zero values on > {percentage} % samples are: "
+            f"{((taxa.shape[0] - (taxa.round(decimals=8) == 0).sum(axis=0)) >= min_number_of_samples).sum()}"
+            f" out of {taxa.shape[1]} genus/features before-filtering.")
+
+    non_rare_columns = taxa.columns[
+        (taxa.shape[0] - (taxa.round(decimals=8) == 0).sum(axis=0)) >= min_number_of_samples]
+    return taxa[non_rare_columns]
+
+
+def plot_rare_taxa_statistics_relative_abundance(taxa, abundance_threshold):
+    plt.rc('xtick', labelsize=6)
+    ((((taxa > abundance_threshold).sum(axis=0)) / taxa.shape[0]) * 100).round(0).sort_values(
+        ascending=True).plot.bar(figsize=(16, 6), title='% samples non-zero value observed, per taxa.')
+    plt.show()
+    ((taxa > abundance_threshold).sum(axis=0)).sort_values(ascending=True).plot.bar(
+        figsize=(16, 6), title='#number of samples non-zero value observed, per taxa.')
+    plt.show()
+    ((taxa > abundance_threshold).sum(axis=0)).plot.hist(
+        title='number of non-zero samples per genus')
+    plt.show()
+
+
+def preprocess_filter_rare_taxa_relative_abundance(taxa: pd.DataFrame, verbose=True, percentage=10,
+                                                   abundance_threshold=0.000001) -> pd.DataFrame:
+    """
+    Preprocess the taxonomy: remove rare taxa.
+    Taxa that was found in <percentage of the samples is being removed.
+    Here, we filter according to non-zero values, on observed abundance, but it's more common to filter to
+    some very small threshold on relative abundance to be less sensitive to sampling noise.
+    If verbose, plot and print statistics.
+
+    :param abundance_threshold: (float) The min relative abundance threshold for a taxa to be included in a sample.
+    :param taxa:                (pd.DataFrame) Observed abundance.
+    :param verbose:             (bool) Whether to plot and print statistics on the data.
+    :param percentage:          (int) The minimal percentage of samples to require.
+    :return:                    (pd.DataFrame) Observed abundance, with rare taxa removed.
+    """
+    min_number_of_samples = int((taxa.shape[0] / 100) * percentage)
+    if verbose:
+        plot_rare_taxa_statistics_relative_abundance(taxa, abundance_threshold)
+        print(
+            f"The number of genus/features that have non-zero values on > {percentage} % samples are: "
+            f"{(((taxa > abundance_threshold).sum(axis=0)) >= min_number_of_samples).sum()}"
+            f" out of {taxa.shape[1]} genus/features before-filtering.")
+
+    non_rare_columns = taxa.columns[
+        ((taxa > abundance_threshold).sum(axis=0)) >= min_number_of_samples]
+    return taxa[non_rare_columns]
+
+
+class TaxonomyNormalizer(ABC):
+    def __init__(self, drop_unknown_taxa=False, unknown_taxa_sample_threshold=1.0):
+        self.drop_unknown_taxa = drop_unknown_taxa
+        self.unknown_taxa_sample_threshold = unknown_taxa_sample_threshold
+
+    @abstractmethod
+    def calculate_relative_abundance(self, observed_abundance):
+        """
+
+        :param observed_abundance: (pd.DataFrame) Observed abundance. Samples x Taxonomy columns.
+        Taxonomy level may very, but usually expected genus/species level of annotation. Taxa that was not
+        identified in this level will be aggregated under the 'unknown' column.
+
+        :return: (pd.DataFrame) relative abundance, samples x Taxonomy columns. Where each sample taxonomy sum to 1,
+        as it represent the relative abundance in the sample.
+        """
+        raise NotImplementedError
+
+    def normalize(self, observed_abundance):
+        """
+
+        :param observed_abundance: (pd.DataFrame) Observed abundance. Samples x Taxonomy columns.
+        Taxonomy level may very, but usually expected genus/species level of annotation. Taxa that was not
+        identified in this level will be aggregated under the 'unknown' column.
+
+        :return: (pd.DataFrame) relative abundance, samples x Taxonomy columns. Where each sample taxonomy sum to 1,
+        as it represent the relative abundance in the sample.
+        If, instance drop_unknown_taxa set to true will drop unknown column.
+        Samples that their unknown taxa is larger than unknown_taxa_sample_threshold will be dropped as well.
+        """
+
+        relative_abundance = self.calculate_relative_abundance(observed_abundance)
+        unknown_taxa_sample_threshold = self.unknown_taxa_sample_threshold
+        # clean samples:
+        relative_abundance = relative_abundance.query('Unknown <= @unknown_taxa_sample_threshold')
+        # drop unknown, and recalculate the relative abundance (if we dropped, aka unknown_taxa_sample_threshold < 1)
+        if self.drop_unknown_taxa:
+            if unknown_taxa_sample_threshold:
+                observed_abundance = observed_abundance.loc[relative_abundance.index, :].drop('Unknown', axis=1)
+                relative_abundance = self.calculate_relative_abundance(observed_abundance)
+                relative_abundance = relative_abundance
+            else:
+                relative_abundance = relative_abundance.drop('Unknown', axis=1)
+        # sanity check:
+        if not (relative_abundance.sum(axis=1).round(decimals=8) == 1).all():
+            raise ValueError("Taxonomy normalization failed. Not in all samples relative abundance summed to 1.")
+        return relative_abundance
+
+
+class NaiveTaxonomyNormalizer(TaxonomyNormalizer):
+    """
+    Naive taxonomy normalizer, sometimes called in the literature TSS - total sum scaling.
+    Each taxon is normalized by the sum of abundance taxa in the sample.
+    """
+    def __init__(self, drop_unknown_taxa=False, unknown_taxa_sample_threshold=1.0):
+        super(NaiveTaxonomyNormalizer, self).__init__(drop_unknown_taxa=drop_unknown_taxa,
+                                                      unknown_taxa_sample_threshold=unknown_taxa_sample_threshold)
+
+    def calculate_relative_abundance(self, observed_abundance):
+        return observed_abundance.divide(observed_abundance.sum(axis=1), axis=0)
